9d2153173a1a92193a34edfdd91f8d24d23c0146
Nertsal <sasha.kudasov04@gmail.com>
2023-08-13 13:05:59 +0300

    Update geng

--- src/assets.rs
+++ src/assets.rs
@@ -17,7 +17,7 @@ fn pixel(texture: &mut ugli::Texture) {
 
 impl Assets {
     pub async fn load(manager: &geng::asset::Manager) -> anyhow::Result<Self> {
-        geng::asset::Load::load(manager, &run_dir().join("assets"))
+        geng::asset::Load::load(manager, &run_dir().join("assets"), &())
             .await
             .context("failed to load assets")
     }
--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,14 @@ fn main() {
 
     let opts: Opts = clap::Parser::parse();
 
-    let geng = Geng::new_with(geng::ContextOptions {
-        title: "Geng Game".to_string(),
-        ..geng::ContextOptions::from_args(&opts.geng)
-    });
+    let mut options = geng::ContextOptions::default();
+    options.window.title = "Geng Game".to_string();
+    options.with_cli(&opts.geng);
 
-    let future = {
-        let geng = geng.clone();
-        async move {
-            let manager = geng.asset_manager();
-            let assets = assets::Assets::load(manager).await.unwrap();
-            game::Game::new(&geng, &Rc::new(assets))
-        }
-    };
-    geng.run_loading(future)
+    Geng::run_with(&options, |geng| async move {
+        let manager = geng.asset_manager();
+        let assets = assets::Assets::load(manager).await.unwrap();
+        let state = game::Game::new(&geng, &Rc::new(assets));
+        geng.run_state(state).await;
+    });
 }
