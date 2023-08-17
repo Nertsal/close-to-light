b6091bc547a3d1ad18a8d98764e9e88fc6f70c50
Nertsal <sasha.kudasov04@gmail.com>
2023-08-17 12:59:41 +0300

    Prepare render

---
+++ src/render/game.rs
@@ -0,0 +1,103 @@
+use super::*;
+
+#[allow(dead_code)]
+pub struct GameRender {
+    geng: Geng,
+    assets: Rc<Assets>,
+    render: Render,
+    util: UtilRender,
+}
+
+impl GameRender {
+    pub fn new(geng: &Geng, assets: &Rc<Assets>) -> Self {
+        Self {
+            geng: geng.clone(),
+            assets: assets.clone(),
+            render: Render::new(geng, assets),
+            util: UtilRender::new(geng, assets),
+        }
+    }
+
+    pub fn get_render_size(&self) -> vec2<usize> {
+        self.render.get_render_size()
+    }
+
+    pub fn draw_world(&mut self, model: &Model, old_framebuffer: &mut ugli::Framebuffer) {
+        let mut framebuffer = self.render.start();
+
+        let camera = &model.camera;
+
+        // Telegraphs
+        for tele in &model.telegraphs {
+            self.util.draw_outline(
+                &tele.light.collider,
+                0.05,
+                COLOR_LIGHT,
+                camera,
+                &mut framebuffer,
+            );
+        }
+
+        // Lights
+        for light in &model.lights {
+            self.util
+                .draw_collider(&light.collider, COLOR_LIGHT, camera, &mut framebuffer);
+        }
+
+        // Player
+        let player = model.player.collider.clone();
+        // player.position += model.player.shake;
+        // self.util
+        //     .draw_collider(&player, COLOR_LIGHT, camera, &mut framebuffer);
+        self.util
+            .draw_outline(&player, 0.05, COLOR_LIGHT, camera, &mut framebuffer);
+
+        self.render.dither(model.real_time);
+        self.render.swap_buffer();
+
+        let aabb = Aabb2::ZERO.extend_positive(old_framebuffer.size().as_f32());
+        draw_texture_fit(
+            self.render.get_buffer(),
+            aabb,
+            vec2(0.5, 0.5),
+            &geng::PixelPerfectCamera,
+            &self.geng,
+            old_framebuffer,
+        );
+    }
+
+    pub fn draw_ui(&mut self, model: &Model, framebuffer: &mut ugli::Framebuffer) {
+        let camera = &geng::PixelPerfectCamera;
+        let screen = Aabb2::ZERO.extend_positive(framebuffer.size().as_f32());
+
+        let font_size = screen.height() * 0.05;
+
+        // Fear meter
+        let fear = Aabb2::point(
+            geng_utils::layout::aabb_pos(screen, vec2(0.5, 0.0)) + vec2(0.0, 1.0) * font_size,
+        )
+        .extend_symmetric(vec2(14.0, 0.0) * font_size / 2.0)
+        .extend_up(font_size);
+        self.geng.draw2d().draw2d(
+            framebuffer,
+            camera,
+            &draw2d::Quad::new(fear.extend_uniform(font_size * 0.1), COLOR_LIGHT),
+        );
+        self.geng
+            .draw2d()
+            .draw2d(framebuffer, camera, &draw2d::Quad::new(fear, COLOR_DARK));
+        self.geng.draw2d().draw2d(
+            framebuffer,
+            camera,
+            &draw2d::Quad::new(
+                fear.extend_symmetric(
+                    vec2(
+                        -model.player.fear_meter.get_ratio().as_f32() * fear.width(),
+                        0.0,
+                    ) / 2.0,
+                ),
+                COLOR_LIGHT,
+            ),
+        );
+    }
+}
--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,6 +1,7 @@
+mod game;
 mod util;
 
-pub use util::*;
+pub use self::{game::*, util::*};
 
 use crate::{assets::Assets, model::*};
 
@@ -12,20 +13,17 @@ use geng_utils::{
 pub const COLOR_LIGHT: Rgba<f32> = Rgba::WHITE;
 pub const COLOR_DARK: Rgba<f32> = Rgba::BLACK;
 
-#[allow(dead_code)]
-pub struct GameRender {
+pub struct Render {
     geng: Geng,
     assets: Rc<Assets>,
-    util: UtilRender,
     double_buffer: (Texture, Texture),
 }
 
-impl GameRender {
+impl Render {
     pub fn new(geng: &Geng, assets: &Rc<Assets>) -> Self {
         Self {
             geng: geng.clone(),
             assets: assets.clone(),
-            util: UtilRender::new(geng, assets),
             double_buffer: {
                 let height = 360;
                 let size = vec2(height * 16 / 9, height);
@@ -44,44 +42,22 @@ impl GameRender {
         std::mem::swap(&mut self.double_buffer.0, &mut self.double_buffer.1);
     }
 
-    pub fn get_render_size(&mut self) -> vec2<usize> {
+    pub fn get_render_size(&self) -> vec2<usize> {
         self.double_buffer.0.size()
     }
 
-    pub fn draw_world(&mut self, model: &Model, framebuffer: &mut ugli::Framebuffer) {
-        let (old_framebuffer, mut framebuffer) = (
-            framebuffer,
-            geng_utils::texture::attach_texture(&mut self.double_buffer.0, self.geng.ugli()),
-        );
+    fn get_buffer(&self) -> &ugli::Texture2d<Rgba<f32>> {
+        &self.double_buffer.0
+    }
+
+    pub fn start(&mut self) -> ugli::Framebuffer {
+        let mut framebuffer =
+            geng_utils::texture::attach_texture(&mut self.double_buffer.0, self.geng.ugli());
         ugli::clear(&mut framebuffer, Some(Rgba::BLACK), None, None);
+        framebuffer
+    }
 
-        let camera = &model.camera;
-
-        // Telegraphs
-        for tele in &model.telegraphs {
-            self.util.draw_outline(
-                &tele.light.collider,
-                0.05,
-                COLOR_LIGHT,
-                camera,
-                &mut framebuffer,
-            );
-        }
-
-        // Lights
-        for light in &model.lights {
-            self.util
-                .draw_collider(&light.collider, COLOR_LIGHT, camera, &mut framebuffer);
-        }
-
-        // Player
-        let mut player = model.player.collider.clone();
-        // player.position += model.player.shake;
-        // self.util
-        //     .draw_collider(&player, COLOR_LIGHT, camera, &mut framebuffer);
-        self.util
-            .draw_outline(&player, 0.05, COLOR_LIGHT, camera, &mut framebuffer);
-
+    fn dither(&mut self, time: Time) {
         let mut other_framebuffer =
             geng_utils::texture::attach_texture(&mut self.double_buffer.1, self.geng.ugli());
 
@@ -91,8 +67,8 @@ impl GameRender {
             ugli::DrawMode::TriangleFan,
             &unit_quad_geometry(self.geng.ugli()),
             ugli::uniforms!(
-                u_time: model.real_time.as_f32(),
-                u_framebuffer_size: framebuffer.size().as_f32(),
+                u_time: time.as_f32(),
+                u_framebuffer_size: self.double_buffer.0.size().as_f32(),
                 u_pattern_size: self.assets.dither1.size().as_f32(),
                 u_texture: &self.double_buffer.0,
                 u_dither1: &self.assets.dither1,
@@ -103,53 +79,6 @@ impl GameRender {
                 blend_mode: Some(ugli::BlendMode::straight_alpha()),
                 ..Default::default()
             },
-        );
-
-        self.swap_buffer();
-
-        let aabb = Aabb2::ZERO.extend_positive(old_framebuffer.size().as_f32());
-        draw_texture_fit(
-            &self.double_buffer.0,
-            aabb,
-            vec2(0.5, 0.5),
-            &geng::PixelPerfectCamera,
-            &self.geng,
-            old_framebuffer,
-        );
-    }
-
-    pub fn draw_ui(&mut self, model: &Model, framebuffer: &mut ugli::Framebuffer) {
-        let camera = &geng::PixelPerfectCamera;
-        let screen = Aabb2::ZERO.extend_positive(framebuffer.size().as_f32());
-
-        let font_size = screen.height() * 0.05;
-
-        // Fear meter
-        let fear = Aabb2::point(
-            geng_utils::layout::aabb_pos(screen, vec2(0.5, 0.0)) + vec2(0.0, 1.0) * font_size,
-        )
-        .extend_symmetric(vec2(14.0, 0.0) * font_size / 2.0)
-        .extend_up(font_size);
-        self.geng.draw2d().draw2d(
-            framebuffer,
-            camera,
-            &draw2d::Quad::new(fear.extend_uniform(font_size * 0.1), COLOR_LIGHT),
-        );
-        self.geng
-            .draw2d()
-            .draw2d(framebuffer, camera, &draw2d::Quad::new(fear, COLOR_DARK));
-        self.geng.draw2d().draw2d(
-            framebuffer,
-            camera,
-            &draw2d::Quad::new(
-                fear.extend_symmetric(
-                    vec2(
-                        -model.player.fear_meter.get_ratio().as_f32() * fear.width(),
-                        0.0,
-                    ) / 2.0,
-                ),
-                COLOR_LIGHT,
-            ),
         );
     }
 }
