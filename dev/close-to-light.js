889d8cf0117e3a5638f0203e75f077e1b85a6492
Nertsal <Nertsal@users.noreply.github.com>
2024-05-30 21:00:46 +0000

    deploy: 57c3d114af5c044b0de59fc8dbd8c964437cddf3

--- dev/close-to-light.js
+++ dev/close-to-light.js
@@ -1655,16 +1655,16 @@ function __wbg_get_imports() {
         const ret = makeClosure(arg0, arg1, 163, __wbg_adapter_32);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper3334 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 1376, __wbg_adapter_46);
+    imports.wbg.__wbindgen_closure_wrapper3378 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 1391, __wbg_adapter_46);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper5754 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 2534, __wbg_adapter_49);
+    imports.wbg.__wbindgen_closure_wrapper5798 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 2549, __wbg_adapter_49);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper5930 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 2629, __wbg_adapter_52);
+    imports.wbg.__wbindgen_closure_wrapper5974 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 2644, __wbg_adapter_52);
         return addHeapObject(ret);
     };
     imports['./snippets/geng-2e04217e96c54cd9/inline3.js'] = __wbg_star0;
