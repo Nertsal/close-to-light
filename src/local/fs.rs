1d45817acfb20c8b694b834c455dd3002238574a
nertsal <nestralnertsal@gmail.com>
2024-06-01 18:34:29 +0300

    change music and level storage

--- src/local/fs.rs
+++ src/local/fs.rs
@@ -6,6 +6,50 @@ use super::*;
 mod native {
     use super::*;
 
+    /// Path to the directory that hold locally saved levels and music.
+    pub fn base_path() -> PathBuf {
+        let exe = std::env::current_exe().expect("Failed to find current exe");
+        let app_name = exe.file_stem().unwrap();
+        if let Some(dirs) =
+            directories::ProjectDirs::from("", "", app_name.to_str().expect("Exe name is invalid"))
+        {
+            return dirs.data_dir().to_path_buf();
+        }
+        if let Some(dir) = exe.parent() {
+            return dir.to_path_buf();
+        }
+        std::env::current_dir().unwrap()
+    }
+
+    pub fn all_music_path() -> PathBuf {
+        base_path().join("music")
+    }
+
+    pub fn all_groups_path() -> PathBuf {
+        base_path().join("levels")
+    }
+
+    pub fn music_path(music: Id) -> PathBuf {
+        all_music_path().join(format!("{}", music))
+    }
+
+    pub fn generate_group_path(group: Id) -> PathBuf {
+        let base_path = all_groups_path();
+        if group == 0 {
+            // Generate a random string until it is available
+            let mut rng = rand::thread_rng();
+            loop {
+                let name: String = (0..5).map(|_| rng.gen_range('a'..='z')).collect();
+                let path = base_path.join(name);
+                if !path.exists() {
+                    return path;
+                }
+            }
+        } else {
+            base_path.join(format!("{}", group))
+        }
+    }
+
     pub fn download_music(id: Id, data: Vec<u8>, info: &MusicInfo) -> Result<()> {
         let path = music_path(id);
         std::fs::create_dir_all(&path)?;
@@ -33,40 +77,6 @@ mod native {
 
 #[cfg(not(target_arch = "wasm32"))]
 pub use native::*;
-
-/// Path to the directory that hold locally saved levels and music.
-pub fn base_path() -> PathBuf {
-    preferences::base_path()
-}
-
-pub fn all_music_path() -> PathBuf {
-    base_path().join("music")
-}
-
-pub fn all_groups_path() -> PathBuf {
-    base_path().join("levels")
-}
-
-pub fn music_path(music: Id) -> PathBuf {
-    all_music_path().join(format!("{}", music))
-}
-
-pub fn generate_group_path(group: Id) -> PathBuf {
-    let base_path = all_groups_path();
-    if group == 0 {
-        // Generate a random string until it is available
-        let mut rng = rand::thread_rng();
-        loop {
-            let name: String = (0..5).map(|_| rng.gen_range('a'..='z')).collect();
-            let path = base_path.join(name);
-            if !path.exists() {
-                return path;
-            }
-        }
-    } else {
-        base_path.join(format!("{}", group))
-    }
-}
 
 impl CachedMusic {
     pub async fn load(manager: &geng::asset::Manager, path: impl AsRef<Path>) -> Result<Self> {
