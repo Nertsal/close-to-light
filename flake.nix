10cf12d9532887e36871cc70848a0cfa776732de
nertsal <nestralnertsal@gmail.com>
2025-10-19 18:22:41 +0300

    cargo update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
-    geng.url = "github:geng-engine/cargo-geng";
+    geng.url = "github:geng-engine/cargo-geng/aa2c2242d7024734f3d5f6e5c9879a19c01b100a";
     geng.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { geng, nixpkgs, systems, self }:
