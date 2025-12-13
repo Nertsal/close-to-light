a0dc919d2c34e8efb28615cac3304b6ff44a876e
Nertsal <Nertsal@users.noreply.github.com>
2025-12-13 13:35:51 +0000

    deploy: b0db216664863e64dffb2826591e848e54038b6e

--- dev/assets/shaders/light.glsl
+++ dev/assets/shaders/light.glsl
@@ -27,13 +27,11 @@ void main() {
     vec4 color = texture2D(u_texture, v_vt);
 
     // Hollow cut
-    // NOTE: comparison not with -1.0 due to floating point imprecision in interpolation.
-    if (u_hollow_cut > -0.999) {
-        color.a = min(
-            min(color.a / (1.0 - u_hollow_cut), 1.0),
-            max((1.0 - color.a - u_hollow_cut) / (1.0 - u_hollow_cut), 0.0)
-        );
-    }
+    float d = color.a * 2.0 / (1.0 - u_hollow_cut); // color.a / hollow_max
+    color.a = min(
+        d, // Outer
+        2.0 - d // Inner
+    );
 
     vec4 m_color = u_color * v_color;
     color.rgb *= m_color.rgb;
