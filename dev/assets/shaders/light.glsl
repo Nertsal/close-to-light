d51cba053e26f8762a0099a358c55acc5cd639de
Nertsal <Nertsal@users.noreply.github.com>
2026-02-25 17:35:38 +0000

    deploy: 82d7d8767f36674fbd4a3b18c489bf29637ab029

---
+++ dev/assets/shaders/light.glsl
@@ -0,0 +1,42 @@
+varying vec4 v_color;
+varying vec2 v_vt;
+
+#ifdef VERTEX_SHADER
+uniform mat3 u_projection_matrix;
+uniform mat3 u_view_matrix;
+uniform mat3 u_model_matrix;
+
+attribute vec2 a_pos;
+attribute vec2 a_vt;
+attribute vec4 a_color;
+
+void main() {
+    v_vt = a_vt;
+    v_color = a_color;
+    vec3 pos = u_projection_matrix * u_view_matrix * u_model_matrix * vec3(a_pos, 1.0);
+    gl_Position = vec4(pos.xy, 0.0, pos.z);
+}
+#endif
+
+#ifdef FRAGMENT_SHADER
+uniform vec4 u_color;
+uniform sampler2D u_texture;
+uniform float u_hollow_cut;
+
+void main() {
+    vec4 color = texture2D(u_texture, v_vt);
+
+    // Hollow cut
+    float d = color.a * 2.0 / (1.0 - u_hollow_cut); // color.a / hollow_max
+    color.a = min(
+        d, // Outer
+        2.0 - d // Inner
+    );
+
+    vec4 m_color = u_color * v_color;
+    color.rgb *= m_color.rgb;
+
+    color = vec4(color.rgb * color.a, m_color.a); // Premultiply alpha
+    gl_FragColor = color;
+}
+#endif
