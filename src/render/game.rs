233530f318ae367a25ce375a5ee9b54866fbc444
nertsal <nestralnertsal@gmail.com>
2026-08-16 14:35:29 +0300

    [ui] fix modifiers ui moving with camera

--- src/render/game.rs
+++ src/render/game.rs
@@ -368,9 +368,8 @@ impl GameRender {
             let position = Aabb2::point(vec2(7.3, 4.5)).extend_uniform(0.3);
             for (i, modifier) in model.level.config.modifiers.iter().enumerate() {
                 let position = position.translate(vec2(-(i as f32), 0.0) * position.size());
-                if let Ok(position) = model
-                    .camera
-                    .world_to_screen(framebuffer.size().as_f32(), position.center())
+                if let Ok(position) =
+                    locked_camera.world_to_screen(framebuffer.size().as_f32(), position.center())
                 {
                     let texture = self.context.assets.get_modifier(modifier);
                     self.ui.draw_subtexture(
